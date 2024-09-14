b30cb076976cea117f51f354cda0c675317e3a99
hamzah-prog <seidahamza1992@gmail.com>
2024-09-14 16:06:28 +0400

    Upload PHP Preview

---
+++ footer.php
@@ -0,0 +1,12 @@
+<style type="text/css">
+<!--
+.style1 {
+	color: #000000;
+	font-weight: bold;
+}
+-->
+</style>
+ <div class="footer">
+          <div class="style1"> <p> Copyright &copy; <?php echo date("Y")?>. CKT-UTAS ONLINE CLEARANCE SYSTEM.  All Rights Reserved. </p>
+</div>
+</div>
